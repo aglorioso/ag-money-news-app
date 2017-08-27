51f640a9d937a16e7dd1f0637a5ef0942c3b2da9
aglorioso <alexandra.glorioso@naplesnews.com>
2017-08-27 17:26:05 -0400

    Created a detail page about each contribution.

--- app.py
+++ app.py
@@ -23,6 +23,14 @@ def index():
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
 
+@app.route('/<row_id>/')
+def detail(row_id):
+	template = 'detail.html'
+	object_list = get_csv()
+	for row in object_list:
+		if row['index'] == row_id:
+			return render_template(template, object=row)
+
 if __name__ == '__main__':
 	#first up the Flask test server
 	app.run(debug=True, use_reloader=True)
